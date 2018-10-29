34450fbe0c60deb34fb9f622970bd1a777a6338f
Jens Langhammer <jens.langhammer@beryju.org>
2018-10-29 16:45:29 +0100

    initial commit

---
+++ pyazo/pyazo/AppDelegate.swift
@@ -0,0 +1,48 @@
+//
+//  AppDelegate.swift
+//  pyazo
+//
+//  Created by Jens Langhammer on 08/10/2018.
+//  Copyright © 2018 Jens Langhammer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
+    let popover = NSPopover()
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        if let button = statusItem.button {
+            button.image = NSImage(named: "MenubarIcon")
+            button.image?.isTemplate = true
+            button.action = #selector(togglePopover(_:))
+        }
+        popover.contentViewController = PyazoMenubarController.freshController()
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    @objc func togglePopover(_ sender: Any?) {
+        if popover.isShown {
+            closePopover(sender: sender)
+        } else {
+            showPopover(sender: sender)
+        }
+    }
+
+    func showPopover(sender: Any?) {
+        if let button = statusItem.button {
+            popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+        }
+    }
+
+    func closePopover(sender: Any?) {
+        popover.performClose(sender)
+    }
+
+}
---
+++ pyazo/pyazo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  pyazo
+//
+//  Created by Jens Langhammer on 08/10/2018.
+//  Copyright © 2018 Jens Langhammer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
