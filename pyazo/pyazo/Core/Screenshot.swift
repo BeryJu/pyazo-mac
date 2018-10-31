132988b3f3aab75832e9ed5d3c631c5012e36a52
Jens Langhammer <jens.langhammer@beryju.org>
2018-10-31 17:19:03 +0100

    add screenshot functionality

--- pyazo/pyazo/Core/Screenshot.swift
+++ pyazo/pyazo/Core/Screenshot.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+
+class Screenshot {
+    
+    var file = ""
+    
+    init() {
+        let tempDir = NSTemporaryDirectory()
+        self.file = URL(fileURLWithPath: tempDir).appendingPathComponent("pyazo_screenshot.png").path
+    }
+
+    func cleanup() {
+        do {
+            try FileManager().removeItem(atPath: self.file)
+        } catch let error {
+            print(error)
+        }
+    }
+    
+    func capture() {
+        let task = Process()
+        task.launchPath = "/usr/sbin/screencapture"
+        task.arguments = ["-i", self.file]
+        task.launch()
+        task.waitUntilExit()
+    }
+    
+}
