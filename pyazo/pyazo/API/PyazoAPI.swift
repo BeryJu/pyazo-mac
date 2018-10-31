323b055c89ff5b083dd129486b8cafed6df33b3f
Jens Langhammer <jens.langhammer@beryju.org>
2018-10-31 16:13:03 +0100

    Add settings support to API and add option to open browser

--- pyazo/pyazo/API/PyazoAPI.swift
+++ pyazo/pyazo/API/PyazoAPI.swift
@@ -11,18 +11,53 @@ import Alamofire
 
 class PyazoAPI {
     
+    static let DEFAULT_SERVER = "i.beryju.org"
+    static let KEY_DO_NOTIFY = "DO_NOTIFY"
+    static let KEY_DO_CLIP = "DO_CLIP"
+    static let KEY_DO_OPEN = "DO_OPEN"
+    static let KEY_SERVER = "SERVER"
+   
+    var doNotify = true
+    var doClip = true
+    var doOpen = true
+    var server = ""
+    
+    init() {
+        let defaults = UserDefaults.standard
+        self.doNotify = defaults.bool(forKey: PyazoAPI.KEY_DO_NOTIFY)
+        self.doClip = defaults.bool(forKey: PyazoAPI.KEY_DO_CLIP)
+        self.doOpen = defaults.bool(forKey: PyazoAPI.KEY_DO_OPEN)
+        self.server = defaults.string(forKey: PyazoAPI.KEY_SERVER) ?? PyazoAPI.DEFAULT_SERVER
+    }
+    
     func showNotification(body: String) -> Void {
-        let notification = NSUserNotification()
-        notification.title = "Pyazo"
-        notification.informativeText = body
-        notification.soundName = NSUserNotificationDefaultSoundName
-        NSUserNotificationCenter.default.deliver(notification)
+        if (self.doNotify) {
+            let notification = NSUserNotification()
+            notification.title = "Pyazo"
+            notification.informativeText = body
+            notification.soundName = NSUserNotificationDefaultSoundName
+            NSUserNotificationCenter.default.deliver(notification)
+        } else {
+            print("Not showing notification since it's disabled")
+        }
     }
     
     func setClipboard(value: String) -> Void {
-        let pasteboard = NSPasteboard.general
-        pasteboard.declareTypes([NSPasteboard.PasteboardType.string], owner: nil)
-        pasteboard.setString(value, forType: NSPasteboard.PasteboardType.string)
+        if (self.doClip) {
+            let pasteboard = NSPasteboard.general
+            pasteboard.declareTypes([NSPasteboard.PasteboardType.string], owner: nil)
+            pasteboard.setString(value, forType: NSPasteboard.PasteboardType.string)
+        } else {
+            print("Not setting clipboard since it's disabled")
+        }
+    }
+    
+    func openURL(url: String) -> Void {
+        if (self.doOpen) {
+            NSWorkspace.shared.open(URL(string: url)!)
+        } else {
+            print("Not opening since it's disabled")
+        }
     }
     
     func upload(url: URL) -> Void {
@@ -31,7 +66,7 @@ class PyazoAPI {
                 multipartFormData.append(url as URL , withName: "imagedata")
                 multipartFormData.append("id".data(using: String.Encoding.utf8, allowLossyConversion: false)!, withName :"id")
             },
-            to: "http://localhost:8000/upload/",
+            to: "https://\(server)/upload/",
             encodingCompletion: { encodingResult in
                 switch encodingResult {
                 case .success(let upload, _, _):
@@ -39,6 +74,7 @@ class PyazoAPI {
                         print(response.result.value as Any)
                         self.showNotification(body: response.result.value!)
                         self.setClipboard(value: response.result.value!)
+                        self.openURL(url: response.result.value!)
                     })
                 case .failure(let encodingError):
                     print(encodingError)
